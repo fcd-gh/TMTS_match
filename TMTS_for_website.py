92e12f9afd6c06b6638a427bdaa4af769903305d
citrango1 <fabiola.citrangolo@gmail.com>
2026-04-20 18:31:29 +0200

    .

--- TMTS_for_website.py
+++ TMTS_for_website.py
@@ -48,7 +48,7 @@ st.write("----------------------------------------------------------")
 
 # DEFAULT_REACTION = SMILES_REACTION
 st.write(' You can either enter your reaction by inserting the SMILES of reactants and products \n in the following boxes')
-col1, col2, col3, col4, col5 = st.columns(5, gap="small")
+col1, col2, col3, col4, col5 = st.columns([2,2,1,2,2], gap="small")
 with col1:
     react1=st.text_input('Reactant 1')
 with col2:
